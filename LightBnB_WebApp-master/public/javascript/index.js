1161846fb749fb0bd40b9aa0f52c903d2ccea89b
Woncheol Kim <icijiy@gmail.com>
2022-08-25 16:55:12 -0700

    add reservation button

--- LightBnB_WebApp-master/public/javascript/index.js
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -2,5 +2,8 @@ $(() => {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
+    $(`.reserve-button`).on('click', function() {
+      const idData = $(this).attr('id').substring(17);
+    })
   });
 });
