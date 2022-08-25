2163f1bd2b51dcd10e5385bd5a2c5da3b99f521b
Woncheol Kim <icijiy@gmail.com>
2022-08-25 13:02:09 -0700

    add getuserwithid & adduser

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -40,7 +40,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`
+    SELECT * FROM users
+    WHERE id = $1
+    `, [id])
+    .then((result) => {
+     console.log(result.rows);
+     return result.rows;
+    })
+    .catch((err) => {
+     console.log(err.message);
+  });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -51,11 +62,22 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`
+  INSERT INTO users(name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `, [user.name, user.email, user.password])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
+addUser('userone', 'userone@gmail.com', 'password');
+
 exports.addUser = addUser;
 
 /// Reservations
